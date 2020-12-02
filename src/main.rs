96d1bfab8c3e79cdc2f84c93cf617118489a59ca
Hugo Peixoto <hugo.peixoto@gmail.com>
2020-12-02 01:30:49 +0000

    Add day1

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+
+fn day1() {
+    let file = File::open("inputs/day1.in").unwrap();
+
+    let lines: Vec<_> = io::BufReader::new(file)
+        .lines()
+        .map(|x| x.unwrap())
+        .map(|x| i32::from_str_radix(&x, 10).unwrap())
+        .collect();
+
+    'part1: for a in lines.iter() {
+        for b in lines.iter() {
+            if a + b == 2020 {
+                println!("{}", a * b);
+                break 'part1;
+            }
+        }
+    }
+
+    'part2: for a in lines.iter() {
+        for b in lines.iter() {
+            for c in lines.iter() {
+                if a + b + c == 2020 {
+                    println!("{}", a * b * c);
+                    break 'part2;
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    day1();
+}
