a0e75564452f739ffcfe1782668a08e8a07f9cd3
Hugo Peixoto <hugo.peixoto@gmail.com>
2020-12-06 07:21:39 +0000

    Clean up day6

--- src/day6.rs
+++ src/day6.rs
@@ -1,35 +1,23 @@
-use regex::Regex;
 use std::fs::read_to_string;
-
-use std::fs::File;
-use std::io::{self, BufRead};
 
 pub fn main() {
     let text = read_to_string("inputs/day6.in").unwrap();
-    let mut count = 0;
-    let mut part2 = 0;
-    for entry in text.split("\n\n") {
-        let people = entry.trim().split("\n").count();
+    let mut p1 = 0;
+    let mut p2 = 0;
+    for entry in text.trim().split("\n\n") {
+        let people = entry.split("\n").count();
 
-        println!("people: {:?}", people);
-
-        let mut chars = entry.replace("\n", "").chars().collect::<Vec<_>>();
-
-        chars.sort();
-        chars.dedup();
-
-        count += chars.len();
-
-        let mut count2 = 0;
         for c in 'a'..='z' {
-            let answers = entry.chars().filter(|x| *x == c).count() == people;
+            let answers = entry.chars().filter(|x| *x == c).count();
 
-            if answers { count2 += 1; }
+            if answers > 0 {
+                p1 += 1;
+            }
+            if answers == people {
+                p2 += 1;
+            }
         }
-
-        println!("c2: {:?}", count2);
-        part2 += count2;
     }
-    println!("p1: {:?}", count);
-    println!("p1: {:?}", part2);
+    println!("{}", p1);
+    println!("{}", p2);
 }
