0a4e534681ff872b50ed86f0a3e785a79ce0a7aa
Hugo Peixoto <hugo.peixoto@gmail.com>
2020-12-12 11:04:06 +0000

    Optimize day11

--- src/day11.rs
+++ src/day11.rs
@@ -1,69 +1,87 @@
 use std::fs::read_to_string;
 
-#[derive(Default, Debug, Clone, PartialEq, Eq)]
-struct Layout {
-    state: Vec<char>,
-    width: i32,
-    height: i32,
-}
-
 const DELTAS: [(i32, i32); 8] = [
     (-1, -1),
-    (-1,  0),
-    (-1,  1),
-    ( 0, -1),
-    ( 0,  1),
-    ( 1, -1),
-    ( 1,  0),
-    ( 1,  1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
 ];
 
-fn next(a: &Layout, b: &mut Layout, neighbors: &Vec<usize>, thresh: usize) -> bool {
+fn next(a: &Vec<bool>, b: &mut Vec<bool>, neighbors: &Vec<usize>, thresh: usize) -> bool {
     let mut changed = false;
-    for i in 0..a.state.len() - 1 {
-        let occupied = neighbors[i*8..(i+1)*8]
+    for i in 1..a.len() {
+        let occupied = neighbors[(i - 1) * 8..i * 8]
             .iter()
-            .filter(|&j| a.state[*j] == '#')
-            .count();
+            .fold(0, |acc, j| acc + a[*j] as usize);
 
-        if a.state[i] == 'L' && occupied == 0 {
-            b.state[i] = '#';
-            changed = true;
-        } else if a.state[i] == '#' && occupied >= thresh {
-            b.state[i] = 'L';
-            changed = true;
+        let flip = if a[i] {
+            occupied >= thresh
         } else {
-            b.state[i] = a.state[i];
-        }
+            occupied == 0
+        };
+
+        b[i] = a[i] ^ flip;
+        changed |= flip;
     }
 
     changed
 }
 
-fn neighbors(layout: &Layout, thresh: usize) -> Vec<usize> {
-    let len = layout.state.len() - 1;
-    let mut ns: Vec<usize> = (0..len*8).collect();
+fn neighbors(state: &Vec<char>, width: usize, _height: usize, indexes: &Vec<usize>) -> Vec<usize> {
+    let mut ns: Vec<usize> = vec![];
+    ns.resize(state.len() * 8, 0);
 
-    for i in 0..len {
-        let ix = i as i32 % layout.width;
-        let iy = i as i32 / layout.width;
+    let w1 = width as i32 + 1;
+    let mut ni = 0;
+    for i in 0..state.len() {
+        if state[i] != 'L' {
+            continue;
+        }
 
-        for (di, delta) in DELTAS.iter().enumerate() {
-            ns[i * 8 + di] = len;
-            for m in 1i32..=(thresh as i32) {
-                let px = ix + m * delta.0;
-                let py = iy + m * delta.1;
-                if (0..layout.width).contains(&px) && (0..layout.height).contains(&py) {
-                    let j = (px + py * layout.width) as usize;
+        for delta in DELTAS.iter() {
+            let j = (i as i32 + delta.0 + w1 * delta.1) as usize;
+            if state[j] == 'L' {
+                ns[ni] = indexes[j];
+            }
+            ni += 1;
+        }
+    }
 
-                    if layout.state[j] != '.' {
-                        ns[i * 8 + di] = j;
+    ns
+}
+
+fn neighbors2(state: &Vec<char>, width: usize, height: usize, indexes: &Vec<usize>) -> Vec<usize> {
+    let mut ns: Vec<usize> = vec![];
+    ns.resize(state.len() * 8, 0);
+
+    let w1 = width as i32 + 1;
+    let mut ni = 0;
+    for i in 0..state.len() {
+        if state[i] != 'L' {
+            continue;
+        }
+        let ix = (i as i32 - 1) % w1;
+        let iy = (i as i32 - 1) / w1 - 1;
+
+        for delta in DELTAS.iter() {
+            for m in 1.. {
+                let px = ix as i32 + m as i32 * delta.0;
+                let py = iy as i32 + m as i32 * delta.1;
+                if (0..width as i32).contains(&px) && (0..height as i32).contains(&py) {
+                    let j = (1 + px + py * w1 + w1) as usize;
+                    if state[j] == 'L' {
+                        ns[ni] = indexes[j];
                         break;
                     }
                 } else {
                     break;
                 }
             }
+            ni += 1;
         }
     }
 
@@ -76,39 +94,43 @@ pub fn main() {
     let width = text.chars().position(|c| c == '\n').unwrap();
     let height = text.len() / (width + 1);
 
-    let mut state: Vec<_> = text.chars().filter(|&c| c != '\n').collect();
-    state.push('.');
+    let mut expanded_state: Vec<char> = vec![];
+    expanded_state.resize((width + 1) * (height + 2) + 1, '.');
 
-    let layout = Layout {
-        state,
-        width: width as i32,
-        height: height as i32,
-    };
-
-    let ns = neighbors(&layout, 1);
-    let mut a = &mut layout.clone();
-    let mut b = &mut layout.clone();
-
-    loop {
-        if !next(a, b, &ns, 4) { break; }
-        let c = b;
-        b = a;
-        a = c;
+    for (i, c) in text.chars().enumerate() {
+        expanded_state[i + width + 1 + 1] = c;
     }
 
-    println!("{}", a.state.iter().filter(|&&c| c == '#').count());
-
-    let ns = neighbors(&layout, layout.state.len());
-    let mut a = &mut layout.clone();
-    let mut b = &mut layout.clone();
-
-    loop {
-        if !next(a, b, &ns, 5) { break; }
-
-        let c = b;
-        b = a;
-        a = c;
+    let mut indexes = vec![];
+    let mut i = 0;
+    for &c in expanded_state.iter() {
+        indexes.push(i + 1);
+        if c == 'L' {
+            i += 1;
+        }
     }
 
-    println!("{}", a.state.iter().filter(|&&c| c == '#').count());
+    let mut compact_state = vec![];
+    compact_state.resize(i + 1, false);
+
+    let ns = neighbors(&expanded_state, width, height, &indexes);
+    let ns2 = neighbors2(&expanded_state, width, height, &indexes);
+
+    let mut a = &mut compact_state.clone();
+    let mut b = &mut compact_state.clone();
+
+    while next(a, b, &ns, 4) {
+        (a, b) = (b, a);
+    }
+
+    println!("{}", a.iter().filter(|&&c| c).count());
+
+    let mut a = &mut compact_state.clone();
+    let mut b = &mut compact_state.clone();
+
+    while next(a, b, &ns2, 5) {
+        (a, b) = (b, a);
+    }
+
+    println!("{}", a.iter().filter(|&&c| c).count());
 }
