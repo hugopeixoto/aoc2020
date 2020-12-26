a1e6de2d78e04eccf977c0a427f65c45b79c5629
Hugo Peixoto <hugo.peixoto@gmail.com>
2020-12-26 18:03:59 +0000

    Optimize day25 further

--- src/bin/day25.rs
+++ src/bin/day25.rs
@@ -6,6 +6,8 @@
 #![feature(test)]
 extern crate test;
 
+use std::collections::*;
+
 pub fn modpow(mut n: u64, mut e: u64, m: u64) -> u64 {
     n = n%m;
     let mut r = 1;
@@ -22,25 +24,56 @@ pub fn modpow(mut n: u64, mut e: u64, m: u64) -> u64 {
     r
 }
 
+pub fn exgcd(a: i64, b: i64) -> (i64, i64, i64) {
+    if b < a {
+        exgcd(b, a)
+    } else { // a < b
+        let (mut old_r, mut r) = (a, b);
+        let (mut old_s, mut s) = (1, 0);
+        let (mut old_t, mut t) = (0, 1);
+
+        while r != 0 {
+            let quotient = old_r / r;
+            (old_r, r) = (r, old_r - quotient * r);
+            (old_s, s) = (s, old_s - quotient * s);
+            (old_t, t) = (t, old_t - quotient * t);
+        }
+
+        (old_r, old_s, old_t)
+    }
+}
+
+
 pub fn encrypt(subject: u64, privkey: u64) -> u64 {
     modpow(subject, privkey, 20201227)
 }
 
-pub fn decrypt(pubkey: u64) -> u64 {
-    let mut loopsize = 0;
-    let subject = 7;
-    let mut value = 1;
+pub fn decrypt(n: u64) -> Option<u64> {
+    const MOD: u64 = 20201227;
+    const ALPHA: u64 = 7;
 
-    loop {
-        if value == pubkey {
-            break;
-        }
+    let m = (n as f64).sqrt().ceil() as usize;
 
-        loopsize += 1;
-        value = (value * subject) % 20201227;
+    let mut alphas = HashMap::new();
+
+    let mut aj = 1;
+    for j in 0..m {
+        alphas.insert(aj, j);
+        aj = (aj * ALPHA) % MOD;
     }
 
-    loopsize
+    let alpha_to_minus_m = modpow(ALPHA, MOD - m as u64 - 1, MOD);
+
+    let mut gamma = n;
+    for i in 0..m as u64 {
+        if let Some(&j) = alphas.get(&gamma) {
+            return Some(i * (m as u64) + j as u64);
+        }
+
+        gamma = (gamma * alpha_to_minus_m) % MOD;
+    }
+
+    None
 }
 
 pub fn day25(input: String) -> (u64, u64) {
@@ -49,7 +82,7 @@ pub fn day25(input: String) -> (u64, u64) {
     let card = lines.next().unwrap().parse::<u64>().unwrap();
     let door = lines.next().unwrap().parse::<u64>().unwrap();
 
-    let enc1 = encrypt(door, decrypt(card));
+    let enc1 = encrypt(door, decrypt(card).unwrap());
 
     (enc1, 0)
 }
